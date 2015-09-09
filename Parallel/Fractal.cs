092ef2074920116946ad4d89bb76020c323b3f44
Andrey1024 <vlasov-andrey@mail.ru>
2015-09-09 21:05:29 +0300

    add basic architecture

---
+++ Parallel/Fractal.cs
@@ -0,0 +1,22 @@
+﻿using System;    
+using System.Numerics;
+
+namespace Parallel
+{
+    interface IFractal
+    {
+        Complex zMin  { get; set; }
+        Complex zMax  { get; set; }
+
+        int StepX { get; set; }
+        int StepY { get; set; }
+
+        int MaxIteraion  { get; set; }
+        int MaxConstalnt { get; set; }
+
+        Func<Complex, Complex> F { get; set; }
+        Func<Complex, Complex, Complex> G { get; set; }
+
+        int[,] Process();
+    }
+}
---
+++ Parallel/Parallel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+
+namespace Parallel
+{
+    public class Parallel : IFractal
+    {
+        public int MaxConstalnt { get; set; }
+        public int MaxIteraion { get; set; }
+
+        public Func<Complex, Complex> F { get; set; }
+        public Func<Complex, Complex, Complex> G { get; set; }
+
+        public Complex zMin { get; set; }
+        public Complex zMax { get; set; }
+
+        public int StepX { get; set; }
+        public int StepY { get; set; }
+
+        public Parallel() { }
+        
+        public int[,] Process()
+        {
+            int[,] result = new int[StepX, StepY];
+
+            for (int i = 0; i < StepX; i++)
+            {
+                for (int j = 0; j < StepY; j++)
+                {
+                    int Iteration = 0;
+                    double Module = 0;
+                    Complex zNext;
+                    Complex z = new Complex();
+
+                    zNext = F(z);
+
+                    while (Iteration < MaxIteraion && Module < MaxConstalnt)
+                    {
+                        zNext = G(zNext, z);
+                        Module = zNext.Magnitude;
+                        Iteration++;
+                    }
+
+                    result[i, j] = Iteration;
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
